f40174f03a15b144618b4cf7239d76ec4c3f2994
sistlasumanth90-dev <sistlasumanth90@gmail.com>
2026-08-11 22:59:57 +0400

    Add Cosine Similarity examples

--- 02-Python Fundamentals/numpy_vectors.py
+++ 02-Python Fundamentals/numpy_vectors.py
@@ -37,3 +37,25 @@ def cosine_similarity (vector_a, vector_b):
 
 print("Cosine Similarity is:", cosine_similarity(dog,puppy))
 
+cat = np.array([0.75,0.65,0.30])
+
+dog = np.array([0.8,0.7,0.2])
+puppy = np.array([0.78,0.72,0.25])
+car = np.array([-0.3,0.1,0.9])
+cat = np.array([0.75,0.65,0.30])
+
+print("Dog vs puppy:", cosine_similarity(dog,puppy))
+print("Dog vs Car:",cosine_similarity(dog, car))
+print("Dog vs Cat:", cosine_similarity(dog, cat))
+
+similarities = {"puppy":cosine_similarity(dog,puppy),
+                "car":cosine_similarity(dog,car), 
+                "cat":cosine_similarity(dog,cat)
+                }
+print("These are the similarities:",similarities)
+most_similar = max(similarities, key=similarities.get)
+print("most similar to dog:",most_similar)
+
+scores = {"A":0.45, "B": 0.96, "C": 0.72}
+print(max(scores,key=scores.get))
+print(max(scores.values()))
